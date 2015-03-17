91c988c765cd4ca0d96c0dc6d088c335e6cae0d4
steveplesha <steveplesha@gmail.com>
2015-03-17 15:13:53 -0500

    Started work on the contact page, added gmaps.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+var map = GMaps({
+    div: '#map',
+    lat: 41.555239,
+    lng: -87.442752
+});
 
+map.addMarker({
+    lat: 41.555239,
+    lng: -87.442752,
+    title: 'TMC',
+    infoWindow: {
+        content: "<p>3542 Highway Ave.<br>Highland, IN 46322</p>"
+    }
+});
